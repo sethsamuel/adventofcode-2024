c25ae44f1d4fce6d4a82143a0df9d3bdfcbc8051
Seth Samuel <sethfsamuel@gmail.com>
2024-12-05 15:29:48 -0500

    Day 2 part 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -14,42 +14,67 @@ pub fn parse_line(line: &str) -> Report {
         .collect()
 }
 
-pub fn is_safe(report: &Report) -> bool {
+/// Returns the index of the unsafe level, or None if the report is safe
+pub fn find_unsafe_level(report: &Report) -> Option<usize> {
     if report.len() < 2 {
-        return true;
+        return None;
     }
     let mut last = &report[0];
-    let is_increasing = report[1].gt(&report[0]);
-    for value in report.iter().skip(1) {
-        if !(1..=3).contains(&value.abs_diff(*last)) {
-            return false;
+    let is_increasing = report[1].ge(last);
+    for (i, current) in report.iter().enumerate().skip(1) {
+        if !(1..=3).contains(&current.abs_diff(*last)) {
+            return Some(i);
         }
-        if is_increasing && value.lt(last) {
-            return false;
+        if is_increasing && current.lt(last) {
+            return Some(i);
         }
-        if !is_increasing && value.gt(last) {
-            return false;
+        if !is_increasing && current.gt(last) {
+            return Some(i);
         }
 
-        last = value
+        last = current
     }
-    true
+    None
 }
 
-pub fn find_safe_count(reports: Reports) -> usize {
-    reports.iter().filter(|r| is_safe(r)).count()
+pub fn is_safe(report: &Report, can_skip: bool) -> bool {
+    let unsafe_level = find_unsafe_level(report);
+    if can_skip && unsafe_level.is_some() {
+        if find_unsafe_level(&report.iter().skip(1).copied().collect()).is_none() {
+            return true;
+        }
+        for skip in unsafe_level.unwrap() - 1..=unsafe_level.unwrap() {
+            if find_unsafe_level(
+                &report
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, v)| (i != skip).then_some(*v))
+                    .collect(),
+            )
+            .is_none()
+            {
+                return true;
+            }
+        }
+    }
+    unsafe_level.is_none()
+}
+
+pub fn find_safe_count(reports: Reports, can_skip: bool) -> usize {
+    reports.iter().filter(|r| is_safe(r, can_skip)).count()
 }
 
 #[allow(dead_code)]
 pub fn part1() {
     let input = read_file(module_path!());
-    println!("{}", find_safe_count(parse_file(input.as_str())));
+    println!("{}", find_safe_count(parse_file(input.as_str()), false));
 }
 
 #[allow(dead_code)]
 pub fn part2() {
-    // let input = read_file(module_path!());
-    // println!("{}", find_id_similarity(parse_file(input.as_str())));
+    let input = read_file(module_path!());
+    let reports = parse_file(input.as_str());
+    println!("{}", find_safe_count(reports, true));
 }
 
 #[cfg(test)]
@@ -79,21 +104,35 @@ mod tests {
     }
 
     #[test]
+    fn test_find_unsafe_level() {
+        assert_eq!(find_unsafe_level(&vec![]), None);
+        assert_eq!(find_unsafe_level(&vec![1]), None);
+        assert_eq!(find_unsafe_level(&vec![1, 2, 4]), None);
+        assert_eq!(find_unsafe_level(&vec![1, 0, 1]), Some(2));
+        assert_eq!(find_unsafe_level(&vec![1, 101, 1]), Some(1));
+        assert_eq!(find_unsafe_level(&vec![1, 3, 2, 4, 5]), Some(2));
+        assert_eq!(find_unsafe_level(&vec![1, 3, 2, 4, 5]), Some(2));
+        assert_eq!(find_unsafe_level(&vec![2, 5, 4, 3, 2]), Some(2));
+        assert_eq!(find_unsafe_level(&vec![2, 1, 3, 4, 5]), Some(2));
+    }
+
+    #[test]
     fn test_is_safe() {
-        assert!(is_safe(&vec![]));
-        assert!(is_safe(&vec![1]));
-        assert!(is_safe(&vec![1, 2, 4]));
-        assert!(!is_safe(&vec![1, 0, 1]));
-        assert!(!is_safe(&vec![1, 101, 1]));
+        // assert!(is_safe(&vec![], false));
+        // assert!(is_safe(&vec![1], false));
+        // assert!(is_safe(&vec![1, 2, 4], false));
+        // assert!(!is_safe(&vec![1, 0, 1], false));
+        // assert!(!is_safe(&vec![1, 101, 1], false));
+        // assert!(!is_safe(&vec![1, 3, 2, 4, 5], false));
+        // assert!(!is_safe(&vec![2, 1, 1, 4, 5], true));
+        // assert!(is_safe(&vec![1, 3, 2, 4, 5], true));
+        // assert!(is_safe(&vec![2, 1, 3, 4, 5], true));
+        assert!(is_safe(&vec![2, 5, 4, 3, 2], true));
     }
 
     #[test]
     fn test_find_safe_count() {
-        assert_eq!(find_safe_count(parse_file(TEST_STR)), 2)
-    }
-
-    #[test]
-    fn test_find_id_similarity() {
-        // assert_eq!(find_id_similarity(parse_file(TEST_STR)), 31)
+        assert_eq!(find_safe_count(parse_file(TEST_STR), false), 2);
+        assert_eq!(find_safe_count(parse_file(TEST_STR), true), 4);
     }
 }
