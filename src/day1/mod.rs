9806f5976b3af1aa86fc7ead7365e5bc78af3ea7
Seth Samuel <sethfsamuel@gmail.com>
2024-12-05 11:48:49 -0500

    Clippy

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,6 +1,72 @@
-use std::{collections::HashMap, iter::Map};
+use std::collections::HashMap;
 
-use crate::file::{self, read_file};
+use crate::file::read_file;
+
+type Id = usize;
+type Ids = Vec<Id>;
+type IdPair = (Id, Id);
+type IdPairs = (Ids, Ids);
+
+pub fn parse_file(text: &str) -> IdPairs {
+    let mut left = Ids::new();
+    let mut right = Ids::new();
+    text.split('\n').map(parse_line).for_each(|p| {
+        left.push(p.0);
+        right.push(p.1);
+    });
+    left.sort();
+    right.sort();
+
+    (left, right)
+}
+
+pub fn parse_line(line: &str) -> IdPair {
+    let ids: Vec<usize> = line
+        .split_whitespace()
+        .map(|d| d.parse::<usize>().unwrap())
+        .collect();
+
+    (*ids.first().unwrap(), *ids.last().unwrap())
+}
+
+pub fn find_id_distance(pairs: IdPairs) -> usize {
+    let mut distance = 0;
+    let left = pairs.0;
+    let right = pairs.1;
+    for i in 0..left.len() {
+        match left[i] > right[i] {
+            true => distance += left[i] - right[i],
+            _ => distance += right[i] - left[i],
+        }
+    }
+    distance
+}
+
+pub fn find_id_similarity(pairs: IdPairs) -> usize {
+    let mut counts: HashMap<Id, usize> = HashMap::new();
+    let mut score = 0;
+    pairs.1.iter().for_each(|id| {
+        counts.insert(*id, counts.get(id).unwrap_or(&0) + 1);
+    });
+
+    pairs
+        .0
+        .iter()
+        .for_each(|id| score += *id * counts.get(id).unwrap_or(&0));
+
+    score
+}
+
+#[allow(dead_code)]
+pub fn part1() {
+    let input = read_file(module_path!());
+    println!("{}", find_id_distance(parse_file(input.as_str())));
+}
+
+pub fn part2() {
+    let input = read_file(module_path!());
+    println!("{}", find_id_similarity(parse_file(input.as_str())));
+}
 
 #[cfg(test)]
 mod tests {
@@ -34,74 +100,4 @@ mod tests {
     fn test_find_id_similarity() {
         assert_eq!(find_id_similarity(parse_file(TEST_STR)), 31)
     }
-}
-
-type Id = usize;
-type Ids = Vec<Id>;
-type IdPair = (Id, Id);
-type IdPairs = (Ids, Ids);
-
-pub fn parse_file(text: &str) -> IdPairs {
-    let mut left = Ids::new();
-    let mut right = Ids::new();
-    text.split("\n")
-        .map(|l| parse_line(l))
-        .into_iter()
-        .for_each(|p| {
-            left.push(p.0);
-            right.push(p.1);
-        });
-    left.sort();
-    right.sort();
-
-    (left, right)
-}
-
-pub fn parse_line(line: &str) -> IdPair {
-    let ids: Vec<usize> = line
-        .split_whitespace()
-        .map(|d| d.parse::<usize>().unwrap())
-        .collect();
-
-    (*ids.first().unwrap(), *ids.last().unwrap())
-}
-
-pub fn find_id_distance(pairs: IdPairs) -> usize {
-    let mut distance = 0;
-    let left = pairs.0;
-    let right = pairs.1;
-    for i in 0..left.len() {
-        if left[i] > right[i] {
-            distance += left[i] - right[i]
-        } else if right[i] > left[i] {
-            distance += right[i] - left[i]
-        }
-    }
-    distance
-}
-
-pub fn find_id_similarity(pairs: IdPairs) -> usize {
-    let mut counts: HashMap<Id, usize> = HashMap::new();
-    let mut score = 0;
-    pairs.1.iter().for_each(|id| {
-        counts.insert(*id, counts.get(id).unwrap_or(&0) + 1);
-    });
-
-    pairs
-        .0
-        .iter()
-        .for_each(|id| score += *id * counts.get(id).unwrap_or(&0));
-
-    score
-}
-
-#[allow(dead_code)]
-pub fn part1() {
-    let input = read_file(module_path!());
-    println!("{}", find_id_distance(parse_file(input.as_str())));
-}
-
-pub fn part2() {
-    let input = read_file(module_path!());
-    println!("{}", find_id_similarity(parse_file(input.as_str())));
 }
