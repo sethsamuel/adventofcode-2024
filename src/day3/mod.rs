3d57dec988219b49cf8821fce9f0da900f54032f
Seth Samuel <sethfsamuel@gmail.com>
2024-12-05 19:39:39 -0500

    Day 3

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,86 +1,60 @@
 use crate::file::read_file;
 
-enum ParseState {
-    Start,
-    M,
-    U,
-    L,
-    Digit,
-    Comma,
-    LeftParen,
-}
-
 type Multiplicand = usize;
 type Multiplicands = Vec<(Multiplicand, Multiplicand)>;
 
-fn reset(buffer: &mut usize, state: &mut ParseState) {
-    *buffer = 0;
-    *state = ParseState::Start
-}
-
 pub fn parse_file(text: &str) -> Multiplicands {
-    //
-    // let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    // re.captures_iter(text)
-    //     .map(|c| c.extract().1.map(|v| v.parse().unwrap()))
-    //     .map(|v: [usize; 2]| (v[0], v[1]))
-    //     .collect()
-    let mut state = ParseState::Start;
     let mut multiplicands = vec![];
 
-    let mut buffer = 0;
-    let mut left: Multiplicand = 0;
+    let mut buffer;
+    let mut is_enabled = true;
 
-    for c in text.chars() {
-        match state {
-            ParseState::Start => match c {
-                'm' => state = ParseState::M,
-                _ => reset(&mut buffer, &mut state),
-            },
-            ParseState::M => match c {
-                'u' => state = ParseState::U,
-                _ => reset(&mut buffer, &mut state),
-            },
-            ParseState::U => match c {
-                'l' => state = ParseState::L,
-                _ => reset(&mut buffer, &mut state),
-            },
-            ParseState::L => match c {
-                '(' => state = ParseState::LeftParen,
-                _ => reset(&mut buffer, &mut state),
-            },
-            ParseState::LeftParen => match c.is_ascii_digit() {
-                true => {
-                    buffer = buffer * 10 + c.to_string().parse::<Multiplicand>().unwrap();
-                    state = ParseState::Digit;
-                }
-                false => reset(&mut buffer, &mut state),
-            },
-            ParseState::Digit => match c.is_ascii_digit() {
-                true => {
-                    buffer = buffer * 10 + c.to_string().parse::<Multiplicand>().unwrap();
-                    state = ParseState::Digit
-                }
-                false => match c {
-                    ',' => {
-                        left = buffer;
-                        buffer = 0;
-                        state = ParseState::Comma
+    let chars: Vec<char> = text.chars().collect();
+    let mut i = 0;
+    while i < chars.len() {
+        let c = chars[i];
+        match c {
+            'm' => {
+                if is_enabled && text[i..i + 4] == *"mul(" {
+                    i += 4;
+                    buffer = 0;
+                    let mut left: Multiplicand = 0;
+                    while i < chars.len() {
+                        let d = chars[i];
+                        if d.is_ascii_digit() {
+                            buffer = buffer * 10 + d.to_string().parse::<Multiplicand>().unwrap()
+                        } else if d == ',' {
+                            if left > 0 {
+                                // Bad comma
+                                break;
+                            }
+                            left = buffer;
+                            buffer = 0;
+                        } else if d == ')' {
+                            multiplicands.push((left, buffer));
+                            break;
+                        } else {
+                            // Invalid character inside mul()
+                            break;
+                        }
+                        i += 1;
                     }
-                    ')' => {
-                        multiplicands.push((left, buffer));
-                        reset(&mut buffer, &mut state);
-                    }
-                    _ => reset(&mut buffer, &mut state),
-                },
-            },
-            ParseState::Comma => match c.is_ascii_digit() {
-                true => {
-                    buffer = buffer * 10 + c.to_string().parse::<Multiplicand>().unwrap();
-                    state = ParseState::Digit;
+                } else {
+                    i += 1
                 }
-                false => reset(&mut buffer, &mut state),
-            },
+            }
+            'd' => {
+                if text[i..i + 4] == *"do()" {
+                    is_enabled = true;
+                    i += 4;
+                } else if text[i..i + 7] == *"don't()" {
+                    is_enabled = false;
+                    i += 7;
+                } else {
+                    i += 1;
+                }
+            }
+            _ => i += 1,
         }
     }
 
@@ -99,9 +73,8 @@ pub fn part1() {
 
 #[allow(dead_code)]
 pub fn part2() {
-    let _input = read_file(module_path!());
-    // let reports = parse_file(input.as_str());
-    // println!("{}", find_safe_count(reports, true));
+    let input = read_file(module_path!());
+    println!("{}", sum_and_multiply(parse_file(input.as_str())));
 }
 
 #[cfg(test)]
@@ -111,14 +84,28 @@ mod tests {
     static TEST_STR: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
+    static TEST_STR2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))\nmul(8,5)";
+
     #[test]
     fn test_parse_file() {
         assert_eq!(parse_file("mul(1,23)"), vec![(1, 23)]);
-        assert_eq!(parse_file(TEST_STR), vec![(2, 4), (5, 5), (11, 8), (8, 5)])
+        assert_eq!(parse_file(TEST_STR), vec![(2, 4), (5, 5), (11, 8), (8, 5)]);
+        assert_eq!(parse_file(TEST_STR2), vec![(2, 4), (8, 5), (8, 5)]);
     }
 
     #[test]
     fn test_sum_and_multiply() {
-        assert_eq!(sum_and_multiply(parse_file(TEST_STR)), 161)
+        assert_eq!(sum_and_multiply(parse_file("mul(123,456)")), 123 * 456);
+        assert_eq!(
+            sum_and_multiply(parse_file("mul(123,456)mul(789,1)")),
+            123 * 456 + 789 * 1
+        );
+        assert_eq!(
+            sum_and_multiply(parse_file("mul(123,456don't()do()mul(789,1)")),
+            789 * 1
+        );
+        assert_eq!(sum_and_multiply(parse_file(TEST_STR)), 161);
+        assert_eq!(sum_and_multiply(parse_file(TEST_STR2)), 88);
     }
 }
