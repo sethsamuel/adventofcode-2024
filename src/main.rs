9e95757e299686fc30f5db892d405e25300ef8bb
Seth Samuel <sethfsamuel@gmail.com>
2024-12-08 13:09:06 -0500

    Day 7 part 1

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
-
+mod day7;
 fn main() {
     // day1::part1();
     // day1::part2();
@@ -19,5 +19,6 @@ fn main() {
     // day5::part1();
     // day5::part2();
     // day6::part1();
-    day6::part2();
+    // day6::part2();
+    day7::part1();
 }
